60c9cef1a91d20a2324ca3d119763ec16a20a268
David Konečný <david@konecny.eu>
2020-12-01 23:57:41 +0100

    new parser, new card generation, design, rules fix

--- data/parseDatasets.js
+++ data/parseDatasets.js
@@ -1,9 +1,15 @@
 const fs = require('fs');
 const datasets = require("./datasets/datasets.json")
 
+const getRandomInt = (min, max) => {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
 datasets.forEach((dataset) => {
     const { id, name, file } = dataset;
-    const finalDataset = {
+    const parsedDataset = {
         name: name,
         id: id,
         questions: [],
@@ -21,14 +27,40 @@ datasets.forEach((dataset) => {
         lines.forEach((line) => {
             const parsedLine = line.split("\t")
             if (parsedLine.length > 1 && parsedLine[1] !== "") {
-                finalDataset.bonuses.push(parsedLine[1])
+                parsedDataset.bonuses.push(parsedLine[1])
             }
-            finalDataset.questions.push(parsedLine[0])
+            parsedDataset.questions.push(parsedLine[0])
         });
     } catch (err) {
         console.error(err);
     }
 
-    const data = JSON.stringify(finalDataset)
+    const cards = []
+
+    // Generate questions
+    let i = 0;
+    while (dataset.questions > 0) {
+        if (i >= parsedDataset.questions.length) i = 0;
+        if (Math.random() < dataset.smallBonusChance)
+            cards.push({ type: 0, text: parsedDataset.questions[i], bonus: parsedDataset.bonuses[getRandomInt(0, parsedDataset.bonuses.length)]})
+        else
+            cards.push({ type: 0, text: parsedDataset.questions[i] })
+        i += 1;
+        dataset.questions -= 1;
+    }
+    parsedDataset.questions = undefined;
+
+    // Generate bonuses
+    i = 0;
+    while (dataset.bonuses > 0) {
+        if (i >= parsedDataset.bonuses.length) i = 0;
+        cards.push({ type: 1, text: parsedDataset.bonuses[i] })
+        i += 1;
+        dataset.bonuses -= 1;
+    }
+    parsedDataset.bonuses = undefined;
+    parsedDataset.cards = cards;
+
+    const data = JSON.stringify(parsedDataset)
     fs.writeFileSync(`./export/${id}.json`, data);
 })
